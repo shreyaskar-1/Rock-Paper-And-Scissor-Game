015fd0b238806e7fa996ffa8ffe8c87a8ee8b97e
shreyaskar-1 <shreyaskar_219031@gmail.com>
2024-02-29 11:02:28 +0530

    addons

--- Script.js
+++ Script.js
@@ -2,7 +2,10 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
-const msg = document.querySelector("#msg")
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
 
 const genCompChoice = () => {
     const options = ["rock", "paper", "scissors"];
@@ -30,10 +33,14 @@ const updateScore = (userWin) => {
 
 const showWinner = (userWin , userChoice , compChoice) => {
     if (userWin){
+        userScore++;
+        userScorePara.innerText = userScore
         console.log("you win!");
         msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green"
     } else {
+        compScore++
+        compScorePara.innerText = compScore
         console.log("you lose");
         msg.innerText = `You lost! ${compChoice} beats your ${userChoice}`;
         msg.style.backgroundColor = "red"
